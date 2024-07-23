1cc3c993e6124fdcf9d9973b2a4e47b19d185d2e
Reece <reecemorgandev@gmail.com>
2024-07-23 09:43:32 +0100

    added provider for redux store

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import BasketPage from "./pages/basket";
 import HomePage from "./pages/home";
 import Layout from "./pages/layout";
 import PageNotFound from "./pages/404";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="basket" element={<BasketPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="basket" element={<BasketPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 

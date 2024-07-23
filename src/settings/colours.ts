3e3705a369a659ed1a371696ca0eeafdf9450bf1
Reece <reecemorgandev@gmail.com>
2024-07-23 10:27:49 +0100

    Update colours.ts

--- src/settings/colours.ts
+++ src/settings/colours.ts
@@ -1,10 +1,11 @@
 const colours = {
   primary: "#FB5858",
-  secondary: "#ffffff",
-  background: "#1f1f1f",
+  secondary: "#ff9933",
+  tertiary: "#009900",
+  background: "#0000ff",
   hover: "#1f1f1f",
   black: "#000000",
-  white: "#ffffff"
+  white: "#ffffff",
 };
 
-export default colours
+export default colours;

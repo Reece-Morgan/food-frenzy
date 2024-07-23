91eee02b55756ffe6359c22a9232255d51d305bf
Reece <reecemorgandev@gmail.com>
2024-07-23 10:02:50 +0100

    added settings files

---
+++ src/settings/breakpoints.ts
@@ -0,0 +1,11 @@
+const breakpoints = {
+  xxs: '320px',
+  xs: '450px',
+  sm: '640px',
+  tablet: '768px',
+  md: '960px',
+  lg: '1200px',
+  xl: '1920px'
+};
+
+export default breakpoints;
---
+++ src/settings/colours.ts
@@ -0,0 +1,10 @@
+const colours = {
+  primary: "#FB5858",
+  secondary: "#ffffff",
+  background: "#1f1f1f",
+  hover: "#1f1f1f",
+  black: "#000000",
+  white: "#ffffff"
+};
+
+export default colours

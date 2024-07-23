265613e8728dbdb2f8d1e84153fe2f0dbd3a3faa
Reece <reecemorgandev@gmail.com>
2024-07-23 10:28:44 +0100

    removed title styled

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -38,15 +38,6 @@ const StyledHeader = styled.header`
   }
 `;
 
-const Title = styled.h1`
-  font-size: 2rem;
-  font-weight: bold;
-  letter-spacing: 0.15rem;
-  text-transform: uppercase;
-  width: fit-content;
-  text-decoration: none;
-`;
-
 const List = styled.ul`
   list-style-type: none;
   margin: 0;

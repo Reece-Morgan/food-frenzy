e1d025d7d01e1676b96591a31d72b6183ff73992
Reece <reecemorgandev@gmail.com>
2024-07-23 10:03:15 +0100

    added margin to layout

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/header";
+import styled from "styled-components";
 
 const Layout = () => {
   return (
-    <>
+    <Wrapper>
       <Header />
 
       <Outlet />
-    </>
+    </Wrapper>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  margin: 0 20px;
+`;
